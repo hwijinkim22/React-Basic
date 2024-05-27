2113d9bf53e28d5cd359fb48764e48e539619877
김휘진 <kim@KIM-hwi-jin.local>
2024-05-27 17:03:24 +0900

    리액트 1강 과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const plusCount = () => { // 더하는 함수
+    setCount(count + 1);
+  }
+
+  const minusCount = () => { // 빼는 함수
+    setCount(count - 1);
+  }
+
+  const resetCount = () => {
+    setCount(0);
+  }
+
   return (
-    <div>App</div>
+    <>
+    <h3>Count: {count}</h3>
+    <button onClick={plusCount}>+</button>
+    <button onClick={minusCount}>-</button>
+    <button onClick={resetCount}>리셋</button>
+    </>
   )
 }
 
