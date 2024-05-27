3b9a29393c46058b61d4bf20d3a82e2cdf6f2b97
김휘진 <kim@KIM-hwi-jin.local>
2024-05-27 17:09:18 +0900

    1강 과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
-  const plusCount = () => { // 더하는 함수
+  const plusCount = () => {
+    // 더하는 함수
     setCount(count + 1);
-  }
+  };
 
-  const minusCount = () => { // 빼는 함수
+  const minusCount = () => {
+    // 빼는 함수
     setCount(count - 1);
-  }
+  };
 
   const resetCount = () => {
     setCount(0);
-  }
+  };
 
   return (
-    <>
-    <h3>Count: {count}</h3>
-    <button onClick={plusCount}>+</button>
-    <button onClick={minusCount}>-</button>
-    <button onClick={resetCount}>리셋</button>
-    </>
-  )
-}
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+    }}>
+      <form style={{
+        textAlign: "center",
+      }}>
+        <h3>Count: {count}</h3>
+        <button type="button" onClick={plusCount}>+</button>
+        <button type="button" onClick={minusCount}>-</button>
+        <button type="button" onClick={resetCount}>리셋</button>
+      </form>
+    </div>
+  );
+};
 
-export default App
+export default App;
